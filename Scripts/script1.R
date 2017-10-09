b507c16abd50b9db232d19cc057f3a613b57f39c
rattraya <ajr10@hood.edu>
2017-10-09 17:58:55 -0400

    Allow for change in filename of Script1.R

--- Scripts/script1.R
+++ Scripts/script1.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 library(cowplot)
+#get arguments
+tmp <- commandArgs(TRUE)
+#make a named list of arguments
+lst <- strsplit(tmp, '=', fixed = TRUE) %>% sappley(`[`, 1)
+#fill in defaults if not given
+if(is.null(lst$out))
+  lst$out <- 'out1.odf'
 set.seed(297834) # pick your own seed
 dat <- data_frame(pred = rnorm(100), resp = pred+ rnorm(100))
-pdf('Users/rattraya/firstRepo/Output/out1.pdf')
+pdf('lst$out')
 ggplot(dat, aes(pred, resp)) + geom_point()
 dev.off()
