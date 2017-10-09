fb3e4652ba65daa15f7ad41f468b661a69a6105c
rattraya <ajr10@hood.edu>
2017-10-09 18:02:16 -0400

    Allow for change of random seed in Script1.R

--- Scripts/script1.R
+++ Scripts/script1.R
@@ -7,7 +7,10 @@ lst <- strsplit(tmp, '=', fixed = TRUE) %>% sappley(`[`, 1)
 #fill in defaults if not given
 if(is.null(lst$out))
   lst$out <- 'out1.odf'
-set.seed(297834) # pick your own seed
+#set.seed(297834) # pick your own seed
+#allow changing of random seed
+if(!is.null(lst$seed))
+  set.seed(as.numeric(lst$seed))
 dat <- data_frame(pred = rnorm(100), resp = pred+ rnorm(100))
 pdf('lst$out')
 ggplot(dat, aes(pred, resp)) + geom_point()
